be0335a17716fc451c2e27f5e6dce12e1b45c78a
DevMikeJan <teyabmichaeljan@gmail.com>
2023-09-11 20:26:00 +0800

    UploadAssets

--- app/models/AssetModel.php
+++ app/models/AssetModel.php
@@ -88,4 +88,47 @@ class AssetModel {
 
         return ($row > 0) ? true : false;
     }
+
+    public function chkExistingFile($conn, $data) {
+        $conn->query('SELECT * FROM ASSETS_UPLOAD WHERE ASSET_RAR_RAN_NUM = :ASSET_RAR_RAN_NUM
+                      OR ASSET_IMG_RAN_NUM = :ASSET_IMG_RAN_NUM OR ASSETFILE_ID = :ASSETFILE_ID');
+
+        $conn->bind(':ASSET_RAR_RAN_NUM', $data['random_number_rar']);
+        $conn->bind(':ASSET_IMG_RAN_NUM', $data['random_number_img']);
+        $conn->bind(':ASSETFILE_ID', $data['asset_file_id']);
+        $conn->execute();
+            
+        //check if the email is already taken
+        return ($conn->rowCount() > 0) ? true : false;
+    }
+
+    public function fileUpload($conn, $data) {
+        $conn->query('INSERT INTO ASSETS_UPLOAD(ASSETFILE_ID, ASSET_NAME, ASSET_DESC, ASSET_FILE_NAME, ASSET_RAR_FILE_EXT,
+                                                ASSET_IMG_FILE_EXT, ASSET_RAR_RAN_NUM, ASSET_IMG_RAN_NUM,ASSET_DEVELOPER_ID,
+                                                ASSET_DEVELOPER_BY, ASSET_UPLOADED_DATE)
+                                        VALUES (:ASSETFILE_ID, :ASSET_NAME, :ASSET_DESC, :ASSET_FILE_NAME, :ASSET_RAR_FILE_EXT,
+                                                :ASSET_IMG_FILE_EXT, :ASSET_RAR_RAN_NUM, :ASSET_IMG_RAN_NUM, :ASSET_DEVELOPER_ID,
+                                                :ASSET_DEVELOPER_BY, :ASSET_UPLOADED_DATE)');
+
+        $conn->bind(':ASSETFILE_ID', $data['asset_file_id']);
+        $conn->bind(':ASSET_NAME', $data['asset_name']);
+        $conn->bind(':ASSET_DESC', $data['asset_desc']);
+        $conn->bind(':ASSET_FILE_NAME', $data['asset_name']);
+        $conn->bind(':ASSET_RAR_FILE_EXT', $data['asset_rar_ext']);
+        $conn->bind(':ASSET_IMG_FILE_EXT', $data['asset_img_ext']);
+        $conn->bind(':ASSET_RAR_RAN_NUM', $data['random_number_rar']);
+        $conn->bind(':ASSET_IMG_RAN_NUM', $data['random_number_img']);
+        $conn->bind(':ASSET_DEVELOPER_ID', $data['uploader_id']);
+        $conn->bind(':ASSET_DEVELOPER_BY', $data['uploader_name']);
+        $conn->bind(':ASSET_UPLOADED_DATE', $data['uploaded_date']);
+        $inserted = $conn->execute();
+
+        if($inserted){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
 }
