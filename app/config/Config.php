1da906a81f3afc3cc00528267cc38602a1e6b825
DevMikeJan <143882967+DevMikeJan@users.noreply.github.com>
2025-06-15 16:10:22 +0800

    Update Config.php

--- app/config/Config.php
+++ app/config/Config.php
@@ -1,30 +1,31 @@
 <?php
 
- //database params
- define('DB_HOST', 'localhost');
- define('DB_USER', 'root');
- define('DB_PASS', '');
- define('DBUSERS', 'MYSITE_USERS');
+//database params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DBUSERS', 'MYSITE_USERS');
 
- //approot
- define('APPROOT', dirname(dirname(__FILE__)));
- define('UPLOADROOT', dirname(dirname(dirname(__FILE__))));
- define('OUTSIDEPROJ', dirname(dirname(dirname(dirname(__FILE__)))));
+//approot
+define('APPROOT', dirname(dirname(__FILE__)));
+define('UPLOADROOT', dirname(dirname(dirname(__FILE__))));
+define('OUTSIDEPROJ', dirname(dirname(dirname(dirname(__FILE__)))));
 
- define("ADMINAPPROOT", dirname(dirname(dirname(__FILE__))));
- //urlroot
- //dynamic links
- define('URLROOT', 'http://localhost/Projects/MySite');
- define('URLROOTADMINSIDE', 'http://localhost/Projects/MySiteAdminSide');
 
- define('ADMINDIRECTLINK',$_SERVER["DOCUMENT_ROOT"] .'/Projects/MySiteAdminSide');
- define('OUTSIDEROOT',$_SERVER["DOCUMENT_ROOT"]);
+define("ADMINAPPROOT", dirname(dirname(dirname(__FILE__))));
+//urlroot
+//dynamic links
+define('URLROOT', 'http://localhost/Projects/MySite');
+define('URLROOTADMINSIDE', 'http://localhost/Projects/MySiteAdminSide');
 
- //what to upload
- define('PROFILEPIC', 'PROFILE PIC');
- define('PROFILECOVER', 'PROFILE COVER');
+define('ADMINDIRECTLINK', $_SERVER["DOCUMENT_ROOT"] . '/Projects/MySiteAdminSide');
+define('OUTSIDEROOT', $_SERVER["DOCUMENT_ROOT"]);
 
- $_SESSION['PREVIOUSPAGE'] = '';
+//what to upload
+define('PROFILEPIC', 'PROFILE PIC');
+define('PROFILECOVER', 'PROFILE COVER');
 
- //sitename
- define('SITENAME', 'MySite');
+$_SESSION['PREVIOUSPAGE'] = '';
+
+//sitename
+define('SITENAME', 'MySite');
